de1c7c036bd1cb7a6644d639000b5b73b903615f
S4URCY <andreshippuden3711@gmail.com>
2019-11-04 21:43:23 -0300

    first commit87

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.use(express.json());
 
 
 server.listen(process.env.PORT || 4201);*/
-
+/*
 const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
@@ -21,4 +21,21 @@ app.get('/*', (req, res) => {
 res.sendFile(path.join(`$/dist/$/index.html`));
 });
  
+app.listen(process.env.PORT || 8080);*/
+
+//Importa as dependências que acabamos de instalar
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/upmusic'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/upmusic/index.html'));
+});
+
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
