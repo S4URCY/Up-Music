29e755753ddf623a133bd3a3c84b20021c164c34
S4URCY <andreshippuden3711@gmail.com>
2019-11-04 21:35:56 -0300

    first commit99

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
+const cors = require('cors');
  
 app.use(express.static(`$/dist/$`));
- 
+server.use(cors());
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`$/dist/$/index.html`));
 });
